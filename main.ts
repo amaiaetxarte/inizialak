95589fe50f6640fb2ade39fdea07109ba91af443
amaiaetxarte <132052285+amaiaetxarte@users.noreply.github.com>
2023-04-28 10:59:44 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    basic.showString("KAIXO")
+    basic.pause(200)
+    basic.showString("AEJ!")
+    basic.pause(200)
+    basic.showString("NAIZ")
+    basic.pause(200)
+})
